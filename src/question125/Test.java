b54390b6ba4fafe3db23a6c94f1b4e8a98423125
PoldiChen <1213386222@qq.com>
2019-04-06 10:09:36 +0800

    add solution for problem 125.

---
+++ src/question125/Test.java
@@ -0,0 +1,11 @@
+package question125;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(str));
+    }
+
+}
