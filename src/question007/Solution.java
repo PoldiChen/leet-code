984aeb878426dca1b7c656e8b5d6172b2e77e806
PoldiChen <1213386222@qq.com>
2019-05-03 10:42:22 +0800

    add codes for question 7.

--- src/question007/Solution.java
+++ src/question007/Solution.java
@@ -8,6 +8,20 @@ package question007;
  **/
 class Solution {
     public int reverse(int x) {
-        return 0;
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10; // 取出最后一位数字
+            x = x / 10; // 原数字减少最后一位
+            if (result > (Integer.MAX_VALUE / 10) || (result == Integer.MAX_VALUE && pop > 7)) {
+//                System.out.println("result1:" + result);
+                return 0;
+            }
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE && pop < -8)) {
+//                System.out.println("result2:" + result);
+                return 0;
+            }
+            result = result * 10 + pop;
+        }
+        return result;
     }
 }
---
+++ src/question007/Test.java
@@ -0,0 +1,15 @@
+package question007;
+
+/**
+ * @author poldi.chen
+ * @className Test
+ * @description TODO
+ * @date 2019/5/2 16:45
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().reverse(1333));
+    }
+
+}
